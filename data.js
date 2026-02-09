fa983325078307657913f02bbaefab1f68ca82a6
raouieddineyounes-sudo <raouieddine.younes@gmail.com>
2026-02-09 19:46:01 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,63 @@
+// C'est ici que vous modifiez les plats et les glucides
+const database = {
+    "Plats": [
+        { name: "Brik à l'œuf (frit)", carb: 20 },
+        { name: "Brik au thon (frit)", carb: 20 },
+        { name: "Chakchouka", carb: 7 },
+        { name: "Chorba", carb: 12 },
+        { name: "Couscous à la viande", carb: 28 },
+        { name: "Couscous au poisson", carb: 28 },
+        { name: "Couscous aux légumes", carb: 25 },
+        { name: "Frites", carb: 35 },
+        { name: "Fricassé", carb: 35 },
+        { name: "Kamounia", carb: 4 },
+        { name: "Kefteji", carb: 8 },
+        { name: "Lablabi (sans pain)", carb: 18 },
+        { name: "Makarouna à la viande", carb: 28 },
+        { name: "Makarouna au poisson", carb: 28 },
+        { name: "Makarouna au poulet", carb: 28 },
+        { name: "Marka à la viande", carb: 6 },
+        { name: "Marka au poulet", carb: 6 },
+        { name: "Merguez", carb: 1 },
+        { name: "Mloukhia", carb: 7 },
+        { name: "Mosli au poisson", carb: 6 },
+        { name: "Mosli au poulet", carb: 6 },
+        { name: "Ojja", carb: 6 },
+        { name: "Pizza", carb: 30 },
+        { name: "Riz Djerbien", carb: 26 },
+        { name: "Salade tunisienne", carb: 4 },
+        { name: "Slata Méchouia", carb: 5 },
+        { name: "Sohleb", carb: 25 },
+        { name: "Tajine Tunisien", carb: 12 }
+    ],
+    "Desserts": [
+        { name: "Assida Zgougou", carb: 40 },
+        { name: "Banane", carb: 20 },
+        { name: "Bsissa", carb: 60 },
+        { name: "Clémentine/Mandarine", carb: 10 },
+        { name: "Compote", carb: 20 },
+        { name: "Dattes", carb: 70 },
+        { name: "Makroudh", carb: 65 },
+        { name: "Melon", carb: 8 },
+        { name: "Orange", carb: 10 },
+        { name: "Pastèque", carb: 7 },
+        { name: "Poire", carb: 13 },
+        { name: "Pomme", carb: 13 },
+        { name: "Raisin", carb: 17 },
+        { name: "Yaourt au fruit", carb: 16 }
+    ],
+    "Divers": [
+        { name: "Abricot sec", carb: 60 },
+        { name: "Amandes", carb: 10 },
+        { name: "Biscuits maison (Sablé)", carb: 65 },
+        { name: "Cacahuètes", carb: 14 },
+        { name: "Cranberries", carb: 80 },
+        { name: "Pain (Baguette)", carb: 55 },
+        { name: "Pain semoule", carb: 50 }
+    ],
+    "Boissons": [
+        { name: "Jus d'orange", carb: 10 },
+        { name: "Jus de fruit (Commerce)", carb: 12 },
+        { name: "Jus de pomme", carb: 11 }
+    ]
+};
